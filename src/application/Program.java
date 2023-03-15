1e37be0d7c4feb285feec7a56ce72e581b3a2747
Luiz Santana <lcpsantana@gmail.com>
2023-03-15 11:44:53 -0300

    Versão Final

---
+++ src/application/Program.java
@@ -0,0 +1,58 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		try {
+			System.out.print("Room number: ");
+			int number = sc.nextInt();
+		
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+		
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+		
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reserva: " + reservation);
+		
+			System.out.println();
+			System.out.println("Insira os dados para atualizar a reserva: ");
+		
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+		
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+		
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reserva: " + reservation);
+		}
+		catch(ParseException e) {
+			System.out.println("Formato de data inválido");
+		}
+		catch(DomainException e) {
+			System.out.println("Erro na reserva: " + e.getMessage());
+			
+		}
+		catch(RuntimeException e) {
+			System.out.println("Erro inesperado");
+			
+		}
+
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Santana
+ *
+ */
+module course {
+}
